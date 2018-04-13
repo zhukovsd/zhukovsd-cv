688b500d48d7369048be1b93bcbf61122acaf9be
Sergey Zhukov <zhukovsd@gmail.com>
2018-04-13 10:14:56 +0300

    Correctly retrieve author email

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ TARGET_BRANCH="gh-pages"
 OUT_DIR="gh-pages"
 
 IP=`ip addr show | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-AUTHOR_EMAIL=`git log -1 --pretty=format:'%an'`
+AUTHOR_EMAIL=`git log -1 --pretty=format:'%ae'`
 
 git config --global user.name "Jenkins@$IP"
 git config --global user.email "$AUTHOR_EMAIL"
