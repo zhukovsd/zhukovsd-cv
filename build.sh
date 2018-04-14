cf6884347f9a9da4a5dadec3a3e6240ddbfcafee
zhukovsd <zhukovsd@gmail.com>
2018-04-14 18:38:50 +0300

    Fixed incorrect call to xelatex from build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-sh 'xelatex cv.tex'
+xelatex cv.tex
 convert -density 500 -resize 1200 -background white -alpha remove cv.pdf cv.png
