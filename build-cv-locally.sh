911ac455673785a1385f0fce679161f8ca9a39c6
Sergey Zhukov <zhukovsd@gmail.com>
2023-02-01 21:08:33 +0400

    CV updated - February 1, 2023

--- build-cv-locally.sh
+++ build-cv-locally.sh
@@ -2,4 +2,4 @@
 
 docker image build -t zhukovsd/cv-builder:latest .
 
-docker container run --rm -v //c/Users/zhukovsd/Documents/GitHub/zhukovsd-cv:/src zhukovsd/cv-builder:latest
+docker container run --rm -v /Users/zhukovsd/Documents/GitHub/zhukovsd-cv:/src zhukovsd/cv-builder:latest
